338ebd8a29bcf48df25de50cd7d26ac35007b185
jacobsanchezbejarano <jake_76847@hotmail.com>
2024-04-30 13:27:54 -0400

    update

--- controllers/email.js
+++ controllers/email.js
@@ -20,7 +20,7 @@ const send_mail = async (req, res, next) => {
 
         // Crear el objeto de opciones del correo electrónico
         const mailOptions = {
-            from: 'Devssoft <process.env.EMAIL_USER_DEVSSOFT>',
+            from: 'Devssoft <'+process.env.EMAIL_USER_DEVSSOFT+'>',
             to: email,
             subject: '¡Gracias por tu interés en nuestros servicios!',
             html: `
@@ -82,8 +82,70 @@ const send_mail = async (req, res, next) => {
         `
         };
 
+        const mailOptions_sendToAdmin = {
+            from: 'Devssoft <'+process.env.EMAIL_USER_DEVSSOFT+'>',
+            to: process.env.EMAIL_USER_DEVSSOFT,
+            subject: 'Solicitu d de Cotización - Devssoft',
+            html: `
+            <html>
+            <head>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        background-color: rgb(14, 100, 150);
+                        margin: 0;
+                        padding: 0;
+                    }
+                    .container {
+                        max-width: 600px;
+                        margin: 0 auto;
+                        padding: 20px;
+                        background-color: #ffffff;
+                        border-radius: 10px;
+                        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                    }
+                    .header {
+                        text-align: center;
+                        margin-bottom: 20px;
+                    }
+                    .header img {
+                        max-width: 200px;
+                        height: auto;
+                    }
+                    .content {
+                        color: rgb(3, 51, 99);
+                        margin-bottom: 20px;
+                    }
+                    .footer {
+                        text-align: center;
+                        color: rgb(10, 102, 194);
+                        font-size: 12px;
+                    }
+                </style>
+            </head>
+            <body>
+                <div class="container">
+                    <div class="header">
+                        <img src="https://devssoft.es/static/media/logo.80dbd328fb96f2e0b3c2.png" alt="Logo de Devssoft">
+                        <h1 style="color: rgb(3, 51, 99);">AUTOMATIZADO</h1>
+                    </div>
+                    <div class="content">
+                        <p>Hola,</p>
+                        <p>Has recibido una solicitud de cotización de parte de ${email}</p>
+                    </div>
+                    <div class="footer">
+                        <p>Saludos cordiales,</p>
+                        <p>Equipo de Devssoft</p>
+                    </div>
+                </div>
+            </body>
+            </html>
+        `
+        };
+
         // Enviar el correo electrónico y esperar la respuesta
         const info = await transporter.sendMail(mailOptions);
+        const info2 = await transporter.sendMail(mailOptions_sendToAdmin);
 
         // Envía una respuesta de éxito al cliente
         // console.log('Correo electrónico enviado: ' + info.response);
